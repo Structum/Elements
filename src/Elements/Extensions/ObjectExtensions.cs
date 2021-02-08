4a4126c6de4b438ce8bb80a04fdc817b7dbe917d
Alex Espinoza <aespinoza@structum.net>
2021-02-07 18:33:24 -0800

    Added DeepClone and Data convert options for objects

--- src/Elements/Extensions/ObjectExtensions.cs
+++ src/Elements/Extensions/ObjectExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.ComponentModel;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Structum.Elements.Extensions
 {
@@ -49,6 +51,71 @@ namespace Structum.Elements.Extensions
             if (val is T) {
                 dictionary.Add(property.Name, (T) val);
             }
+        }
+
+        /// <summary>
+        ///     Creates a deep cloned of the selected object.
+        /// </summary>
+        /// <param name="obj">Object to Clone.</param>
+        /// <typeparam name="T">Type of the object to clone.</typeparam>
+        /// <returns>Cloned object.</returns>
+        public static T DeepClone<T>(this T obj)
+        {
+            using (var ms = new MemoryStream()) {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                ms.Position = 0;
+
+                return (T) formatter.Deserialize(ms);
+            }
+        }
+
+        /// <summary>
+        ///		Converts the selected object as a Guid.
+        /// </summary>
+        /// <param name="dataToConvert">Data to convert.</param>
+        /// <returns>Guid instance if convert was successful, Guid.Empty otherwise.</returns>
+        public static Guid AsGuid(this object dataToConvert)
+        {
+            if(dataToConvert == null) {
+                return Guid.Empty;
+            }
+
+            string dataAsString = dataToConvert.ToString();
+            if(dataAsString == "System.Guid") {
+                return (Guid) dataToConvert;
+            }
+
+            return !Guid.TryParse (dataAsString, out var convertedData) ? Guid.Empty : convertedData;
+        }
+
+        /// <summary>
+        ///		Converts the selected object as a String.
+        /// </summary>
+        /// <param name="dataToConvert">Data to convert.</param>
+        /// <returns>String instance if convert was successful, Guid.Empty otherwise.</returns>
+        public static string AsString(this object dataToConvert)
+        {
+            return dataToConvert == null ? string.Empty : dataToConvert as string;
+        }
+
+        /// <summary>
+        ///		Converts the selected object as a Boolean.
+        /// </summary>
+        /// <param name="dataToConvert">Data to convert.</param>
+        /// <returns>Boolean instance if convert was successful, Guid.Empty otherwise.</returns>
+        public static bool AsBoolean(this object dataToConvert)
+        {
+            if(dataToConvert == null) {
+                return false;
+            }
+
+            string dataAsString = dataToConvert.ToString();
+            if(dataAsString == "System.Boolean") {
+                return (bool) dataToConvert;
+            }
+
+            return bool.TryParse(dataAsString, out var convertedData) && convertedData;
         }
     }
 }
